238f7495fa19fdb443887ba5f60722fd445a396b
Piotr Kozak <piotr5kozak@gmail.com>
2022-09-10 11:07:12 +0200

    Added reactivity to cells
    Cells now respond to clicks and are properly styled.

--- src/components/cell.tsx
+++ src/components/cell.tsx
@@ -1,5 +1,53 @@
-const Cell = ({ state, onClick }: CellProps) => {
-	return <div className="cell" onClick={() => onClick()}></div>;
+const Cell = ({ state, isMine, neigbors, onClick }: CellProps) => {
+	const getCellContnet = (state: CellState) => {
+		if (state === "hidden") return "";
+		if (state === "flagged") return "🚩";
+		if (isMine) return "💣";
+		return neigbors || "";
+	};
+
+	const getCellClass = () => {
+		if (isMine && state === "hidden") return "";
+		if (state === "revealed" && neigbors === 0) return "filled empty";
+		if (state === "revealed" && neigbors) return "filled";
+	};
+
+	return (
+		<div
+			className={`cell ${getCellClass()}`}
+			style={{ color: getFontColor(neigbors) }}
+			onClick={(e) => onClick(e.button)}
+			onContextMenu={(e) => {
+				e.preventDefault();
+				onClick(2);
+			}}
+		>
+			{getCellContnet(state)}
+		</div>
+	);
+};
+
+const getFontColor = (neigbors: number) => {
+	switch (neigbors) {
+		case 1:
+			return "blue";
+		case 2:
+			return "green";
+		case 3:
+			return "red";
+		case 4:
+			return "purple";
+		case 5:
+			return "maroon";
+		case 6:
+			return "turquoise";
+		case 7:
+			return "black";
+		case 8:
+			return "grey";
+		default:
+			return "black";
+	}
 };
 
 export interface CellData {
@@ -11,7 +59,9 @@ export interface CellData {
 
 interface CellProps {
 	state: CellState;
-	onClick: () => void;
+	neigbors: number;
+	isMine: boolean;
+	onClick: (button: number) => void;
 }
 
 export type CellState = "hidden" | "flagged" | "revealed";
--- src/components/grid.tsx
+++ src/components/grid.tsx
@@ -24,19 +24,84 @@ const Grid = ({ mines }: { mines: number }) => {
 		setData(grid);
 	}, [mines, size]);
 
+	const handleCellClick = (cell: CellData, button: number) => {
+		//Left click
+		if (button === 0) {
+			if (cell.state !== "hidden") return;
+
+			const newGrid = [...data];
+			if (cell.isMine) {
+				//Todo: Handle game over
+				newGrid[cell.y][cell.x].state = "revealed";
+			} else {
+				newGrid[cell.y][cell.x].state = "revealed";
+			}
+
+			setData(newGrid);
+		} else if (button === 2) {
+			if (cell.state === "revealed") return;
+
+			const newGrid = [...data];
+			newGrid[cell.y][cell.x].state = cell.state === "hidden" ? "flagged" : "hidden";
+
+			setData(newGrid);
+		}
+	};
+
 	return (
 		<div>
 			{data.map((row, rowIndex) => {
 				return (
 					<div className="row" key={rowIndex}>
 						{row.map((cell, colIndex) => {
-							return <Cell state={cell.state} onClick={() => {}} key={`${rowIndex}-${colIndex}`} />;
+							return (
+								<Cell
+									state={cell.state}
+									onClick={(button) => handleCellClick(cell, button)}
+									key={`${rowIndex}-${colIndex}`}
+									neigbors={getCellNeighbors(cell, data).filter((neighbor) => neighbor.isMine).length}
+									isMine={cell.isMine}
+								/>
+							);
 						})}
 					</div>
 				);
 			})}
 		</div>
 	);
+};
+
+//Reveal cells around the clicked cell without neighbors nearby using recursion
+const revealNeighbors = (cell: CellData, grid: CellData[][]) => {
+	const newGrid = [...grid];
+	newGrid[cell.y][cell.x].state = "revealed";
+
+	const neighbors = getCellNeighbors(cell, grid);
+	for (let neighbor of neighbors) {
+		if (neighbor.state === "hidden" && !neighbor.isMine) {
+			const neighborNeighbors = getCellNeighbors(neighbor, grid);
+			if (neighborNeighbors.filter((neighbor) => neighbor.isMine).length === 0) {
+				revealNeighbors(neighbor, newGrid);
+			}
+		}
+	}
+
+	return newGrid;
+};
+
+//Get the neighbors of a cell
+const getCellNeighbors = (cell: CellData, data: CellData[][]): CellData[] => {
+	const neighbors: CellData[] = [];
+	//Get the cells above
+	for (let y = cell.y - 1; y <= cell.y + 1; y++) {
+		for (let x = cell.x - 1; x <= cell.x + 1; x++) {
+			if (y >= 0 && y < data.length && x >= 0 && x < data[0].length && !(y === cell.y && x === cell.x)) {
+				neighbors.push(data[y][x]);
+			}
+		}
+	}
+
+	return neighbors;
 };
 
 export default Grid;
